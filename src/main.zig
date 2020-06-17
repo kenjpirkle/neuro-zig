27ca069387a9d87089edbe2b12d3f1f93adeb4dc
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-17 20:52:51 +0100

    Modify main.zig to read shader from file

--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,46 @@
 const std = @import("std");
-const c = @cImport({
-    @cDefine("_NO_CRT_STDIO_INLINE", "1");
-    @cInclude("stdio.h");
-    @cInclude("glad.h");
-    @cInclude("glfw3.h");
-});
+const shader = @import("shader.zig");
+usingnamespace @import("c.zig");
 
 pub fn main() anyerror!void {
-    _ = c.printf("hello\n");
+    _ = printf("hello\n");
 
-    if (c.glfwInit() == 0) {
+    if (glfwInit() == 0) {
         std.debug.warn("could not initialize glfw\n", .{});
         return error.GLFWInitFailed;
     }
-    defer c.glfwTerminate();
+    defer glfwTerminate();
 
-    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
-    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 6);
-    c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    const vid_mode = c.glfwGetVideoMode(c.glfwGetPrimaryMonitor());
+    const vid_mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
-    const window = c.glfwCreateWindow(
+    const window = glfwCreateWindow(
         @divTrunc(vid_mode.*.width, 2),
         @divTrunc(vid_mode.*.height, 2),
         "neuro-zig",
         null,
         null
     ) orelse return error.GlfwCreateWindowFailed;
-    defer c.glfwDestroyWindow(window);
+    defer glfwDestroyWindow(window);
 
-    while (c.glfwWindowShouldClose(window) == 0) {
-        c.glfwPollEvents();
+    glfwMakeContextCurrent(window);
+
+    if (gladLoadGLLoader(@ptrCast(GLADloadproc, glfwGetProcAddress)) == 0) {
+        std.debug.warn("could not initialize glad\n", .{});
+        return;
+    }
+
+    const id = shader.Shader.loadShader(
+        "shaders\\vertex.glsl",
+        GL_VERTEX_SHADER
+    );
+
+    std.debug.warn("shader id: {} \n", .{id});
+
+    while (glfwWindowShouldClose(window) == 0) {
+        glfwPollEvents();
     }
 }
