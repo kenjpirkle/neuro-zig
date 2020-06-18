08ba8112306da1809ebc27f284cdb340a57d61f1
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-18 16:15:59 +0100

    Add work on shader to compile glsl shaders

--- src/shader.zig
+++ src/shader.zig
@@ -1,42 +1,40 @@
 const std = @import("std");
 usingnamespace @import("c.zig");
-const Allocator = std.mem.Allocator;
 
-const ShaderList = std.ArrayList(ShaderSource);
 const IdList = std.ArrayList(GLuint);
 
-const ShaderType = enum {
-    Vertex,
-    Fragment,
-};
-
-const ShaderSource = struct {
-    type: ShaderType,
+pub const ShaderSource = struct {
+    shader_type: c_uint,
     source: []const u8,
 };
 
 pub const Shader = struct {
-    pub fn init(shaders: ShaderList) Shader {
-        var ids = IdList.init(allocator);
+    program: GLuint,
+
+    pub fn init(shaders: var) !Shader {
+        var ids = IdList.init(std.heap.page_allocator);
         defer ids.deinit();
         const program = glCreateProgram();
-        for (shaders.toSlice()) |shader| {
-            const new_id = ids.addOne();
+        for (shaders) |shader| {
+            const id = try ids.addOne();
+            id.* = try loadShader(shader);
+            glAttachShader(program, id.*);
         }
-        return Shader {
 
-        };
+        glLinkProgram(program);
+
+        for (ids.items) |id| {
+            glDetachShader(program, id);
+            glDeleteShader(id);
+        }
+
+        return Shader{ .program = program };
     }
 
-    pub fn loadShader(file: []const u8, shader_type: GLenum) !GLuint {
-        std.debug.warn("path: {}\n", .{file});
-        const data = try std.fs.cwd().readFileAlloc(
-            std.heap.page_allocator,
-            file,
-            std.math.maxInt(u64)
-        );
+    fn loadShader(shader_source: ShaderSource) !GLuint {
+        const data = try std.fs.cwd().readFileAlloc(std.heap.page_allocator, shader_source.source, 1024 * 1024 * 1024);
 
-        const shader_id = glCreateShader(shader_type);
+        const shader_id = glCreateShader(shader_source.shader_type);
         glShaderSource(shader_id, 1, @ptrCast([*c]const [*c]const u8, &data), null);
         glCompileShader(shader_id);
 
