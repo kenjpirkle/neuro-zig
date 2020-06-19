386940b56019c1e3891a5c948f1bb6c19473596c
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-19 16:22:01 +0100

    Add quick freetype tests to main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,30 @@
 const std = @import("std");
 const warn = std.debug.warn;
-usingnamespace @import("database.zig");
-usingnamespace @import("user_interface.zig");
+const Database = @import("database.zig").Database;
+const ui = @import("user_interface.zig");
 usingnamespace @import("c.zig");
 
 pub fn main() anyerror!void {
+    var ft: FT_Library = undefined;
+    if (FT_Init_FreeType(&ft) != 0) {
+        warn("could not initialize FreeType library\n", .{});
+        return error.FreeTypeLibraryFailed;
+    }
+    defer _ = FT_Done_FreeType(ft);
+
+    var face: FT_Face = undefined;
+    const font_face = "fonts/Lato/Lato-Light.ttf";
+    if (FT_New_Face(ft, font_face, 0, &face) != 0) {
+        warn("could not load font {}\n", .{font_face});
+        return error.FreeTypeLoadFaceFailed;
+    }
+    warn("successfully loaded {}\n", .{font_face});
+    defer _ = FT_Done_Face(face);
+
     var database = try Database.init("C:/Users/kenny/Desktop/neuro.db");
     defer database.deinit();
 
-    var ui = try UserInterface.init();
+    try ui.init();
     defer ui.deinit();
 
     while (glfwWindowShouldClose(ui.window) == 0) {
