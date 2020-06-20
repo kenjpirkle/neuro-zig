3516872797e03fb2c411e9b3479072ba9df03456
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-20 15:10:10 +0100

    Refactor user_interface

--- src/user_interface.zig
+++ src/user_interface.zig
@@ -1,19 +1,23 @@
+const std = @import("std");
 const allocator = @import("std").heap.c_allocator;
 const SegmentedList = @import("std").SegmentedList;
-const Shader = @import("shader.zig").Shader;
-const ShaderSource = @import("shader.zig").ShaderSource;
+const QuadShader = @import("shaders/quad_shader.zig").QuadShader;
 const Widget = @import("widgets/widget.zig").Widget;
 usingnamespace @import("print.zig");
 usingnamespace @import("c.zig");
 
-const UserInterfaceState = packed struct {};
+const UserInterfaceState = packed struct {
+    draw_required: bool,
+    animating: bool,
+    animating_locked: bool,
+};
 
 pub var window: *GLFWwindow = undefined;
 var video_mode: *const GLFWvidmode = undefined;
-var cursor: ?*GLFWcursor = undefined;
+var cursor: *GLFWcursor = undefined;
 var widgets: SegmentedList(Widget, 32) = undefined;
 var search_bar: u16 = undefined;
-var shader: Shader = undefined;
+var quad_shader: QuadShader = undefined;
 
 pub fn init() !void {
     if (glfwInit() == 0) {
@@ -21,49 +25,34 @@ pub fn init() !void {
         return error.GLFWInitFailed;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    setWindowHints();
 
     video_mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
     const width = @divTrunc(video_mode.*.width, 2);
     const height = @divTrunc(video_mode.*.height, 2);
     window = glfwCreateWindow(width, height, "neuro-zig", null, null) orelse return error.GlfwCreateWindowFailed;
-    cursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR);
+    cursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR) orelse return error.GlfwCreateCursorFailed;
     widgets = SegmentedList(Widget, 32).init(allocator);
 
     glfwMakeContextCurrent(window);
+
+    setGlfwState();
 
     if (gladLoadGLLoader(@ptrCast(GLADloadproc, glfwGetProcAddress)) == 0) {
         printLine("could not initialize glad");
         return error.GladLoadProcsFailed;
     }
 
-    shader = try Shader.init([_]ShaderSource{
-        .{
-            .shader_type = GL_VERTEX_SHADER,
-            .source = "shaders/vertex.glsl",
-        },
-        .{
-            .shader_type = GL_FRAGMENT_SHADER,
-            .source = "shaders/fragment.glsl",
-        },
-    });
+    const version: [*:0]const u8 = glGetString(GL_VERSION);
+    print("OpenGL version: ");
+    printLine(version);
 
-    glfwSwapInterval(1);
-    glViewport(0, 0, width, height);
-    glfwSetWindowSizeLimits(window, 500, 200, GLFW_DONT_CARE, GLFW_DONT_CARE);
-    _ = glfwSetWindowSizeCallback(window, onWindowSizeChanged);
-    _ = glfwSetKeyCallback(window, onKeyEvent);
+    if (glfwExtensionSupported("GL_ARB_bindless_texture") == GLFW_TRUE) {
+        printLine("GL_ARB_bindless_texture is supported!");
+    }
 
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glEnable(GL_DEPTH_TEST);
-    glClearDepth(0.0);
-    glDepthFunc(GL_GEQUAL);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glClearColor(0.25, 0.23, 0.25, 1.0);
+    quad_shader = try QuadShader.init(width, height);
+    setGlState(width, height);
 }
 
 pub fn deinit() void {
@@ -76,6 +65,31 @@ pub fn display() void {
     glClear(GL_COLOR_BUFFER_BIT);
     glfwSwapBuffers(window);
     glfwPollEvents();
+}
+
+inline fn setWindowHints() void {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+inline fn setGlState(window_width: c_int, window_height: c_int) void {
+    glViewport(0, 0, window_width, window_height);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glEnable(GL_DEPTH_TEST);
+    glClearDepth(0.0);
+    glDepthFunc(GL_GEQUAL);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glClearColor(0.25, 0.23, 0.25, 1.0);
+}
+
+inline fn setGlfwState() void {
+    glfwSwapInterval(1);
+    glfwSetWindowSizeLimits(window, 500, 200, GLFW_DONT_CARE, GLFW_DONT_CARE);
+    _ = glfwSetWindowSizeCallback(window, onWindowSizeChanged);
+    _ = glfwSetKeyCallback(window, onKeyEvent);
 }
 
 fn onWindowSizeChanged(win: ?*GLFWwindow, width: c_int, height: c_int) callconv(.C) void {
