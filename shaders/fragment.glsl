d0ffd9542b8613910e9de8e508b006ae0116845a
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-22 17:52:37 +0100

    Implement initial quad shaders with persistent mapped buffers

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,8 +1,30 @@
 #version 460 core
 
+#extension GL_ARB_bindless_texture : require
+
 in vec4 colour;
-out vec4 fb_colour;
+in vec2 tex_coords;
+in flat uint is_text;
+
+layout (early_fragment_tests) in;
+layout (pixel_center_integer, origin_upper_left) in vec4 gl_FragCoord;
+
+// using this causes a segfault atm
+layout (std140) uniform TEXTURE_BLOCK {
+    sampler2D textures[128];
+};
+
+out vec4 out_colour;
+
+float rand(vec2 uv) {
+    return fract(sin(dot(uv, vec2(12.9898, 78.233))) * 43758.5453123);
+}
 
 void main() {
-    fb_colour = colour;
+    const float r = rand(gl_FragCoord.xy) * 0.0125;
+    const vec4 sampled = vec4(1.0, 1.0, 1.0, texture(textures[0], tex_coords).r);
+
+    // using this causes a segfault atm
+    // out_colour = is_text == 1 ? (colour * sampled) : is_text == 2 ? texture(textures[1], tex_coords) : colour - r;
+    out_colour = vec4(0.3, 0.1, 0.3, 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,12 +1,44 @@
 #version 460 core
 
+#extension GL_ARB_bindless_texture : require
+
+layout (location = 0) in vec4 instance_transform;
+layout (location = 1) in float instance_depth;
+layout (location = 2) in uint in_character;
+layout (location = 3) in uvec4 colour_indices;
+
+layout (std430, binding = 0) buffer COLOUR_BLOCK {
+    vec4 in_colours[];
+};
+
 uniform float window_height;
 uniform vec2 res_multi;
+uniform vec4 texture_transforms[128];
 
 out vec4 colour;
+out vec2 tex_coords;
+out flat uint is_text;
+
+const uvec2 vertices[4] = {
+    uvec2(0, 0),
+    uvec2(0, 1),
+    uvec2(1, 0),
+    uvec2(1, 1)
+};
 
 void main() {
-    const float x = 5.0 * res_multi.x;
-    const float y = 5.0 * res_multi.y;
-    colour = vec4(x, y, 0.75, 1.0) * window_height;
+    is_text = (in_character == 0) ? 0 : (in_character == 1) ? 2 : 1;
+
+    const float x = (instance_transform.x + (instance_transform.z * vertices[gl_VertexID].x)) * res_multi.x - 1.0;
+    const float y = (window_height - instance_transform.y - (instance_transform.w * vertices[gl_VertexID].y)) * res_multi.y - 1.0;
+    gl_Position = vec4(x, y, instance_depth, 1);
+    colour = in_colours[colour_indices[gl_VertexID]];
+    const vec4 c = texture_transforms[in_character];
+    if (in_character > 1) {
+        tex_coords.x = (c.x + (c.z * vertices[gl_VertexID].x));
+        tex_coords.y = (c.y + (c.w * vertices[gl_VertexID].y));
+    } else if (in_character == 1) {
+        tex_coords.x = vertices[gl_VertexID].x;
+        tex_coords.y = vertices[gl_VertexID].y;
+    }
 }
