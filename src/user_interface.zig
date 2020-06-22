af61ec841fbadb7d9f252a8af93bbf63ce6b6ae5
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-22 23:12:50 +0100

    Add callbacks and partial implementation to user_interface
    Add widget_with_cursor and widget_with_focus pointers.
    Add cursor position callback, file drop callback and mouse button callback

--- src/user_interface.zig
+++ src/user_interface.zig
@@ -4,6 +4,7 @@ const allocator = @import("std").heap.c_allocator;
 const SegmentedList = @import("std").SegmentedList;
 const QuadShader = @import("shaders/quad_shader.zig").QuadShader;
 const Widget = @import("widgets/widget.zig").Widget;
+const SearchBar = @import("widgets/search_bar.zig").SearchBar;
 usingnamespace @import("c.zig");
 
 const UserInterfaceState = packed struct {
@@ -19,6 +20,9 @@ var widgets: SegmentedList(Widget, 32) = undefined;
 var search_bar: u16 = undefined;
 var quad_shader: QuadShader = undefined;
 
+var widget_with_cursor: ?*Widget = undefined;
+var widget_with_focus: ?*Widget = undefined;
+
 pub fn init() !void {
     if (glfwInit() == 0) {
         warn("could not initialize glfw\n", .{});
@@ -33,6 +37,9 @@ pub fn init() !void {
     window = glfwCreateWindow(width, height, "neuro-zig", null, null) orelse return error.GlfwCreateWindowFailed;
     cursor = glfwCreateStandardCursor(GLFW_ARROW_CURSOR) orelse return error.GlfwCreateCursorFailed;
     widgets = SegmentedList(Widget, 32).init(allocator);
+    widget_with_cursor = null;
+    widget_with_focus = null;
+    try widgets.push(Widget{ .SearchBar = SearchBar{ .parent = null } });
 
     glfwMakeContextCurrent(window);
 
@@ -90,7 +97,9 @@ inline fn setGlfwState() void {
     glfwSwapInterval(1);
     glfwSetWindowSizeLimits(window, 500, 200, GLFW_DONT_CARE, GLFW_DONT_CARE);
     _ = glfwSetWindowSizeCallback(window, onWindowSizeChanged);
+    _ = glfwSetCursorPosCallback(window, onCursorPositionChanged);
     _ = glfwSetKeyCallback(window, onKeyEvent);
+    _ = glfwSetDropCallback(window, onDrop);
 }
 
 fn onWindowSizeChanged(win: ?*GLFWwindow, width: c_int, height: c_int) callconv(.C) void {
@@ -101,6 +110,40 @@ fn onWindowSizeChanged(win: ?*GLFWwindow, width: c_int, height: c_int) callconv(
     while (iter.next()) |widget| {
         widget.onWindowSizeChanged(width, height);
     }
+}
+
+fn onCursorPositionChanged(win: ?*GLFWwindow, x_pos: f64, y_pos: f64) callconv(.C) void {
+    const x = @floatToInt(u16, x_pos);
+    const y = @floatToInt(u16, y_pos);
+
+    if (widget_with_cursor) |w| {
+        if (w.containsPoint(x, y)) {
+            // animations here
+        } else {
+            w.onCursorLeave();
+            widget_with_cursor = findWidgetWithCursor(x, y);
+            if (widget_with_cursor) |new_w| {
+                new_w.onCursorEnter();
+            }
+        }
+    } else {
+        widget_with_cursor = findWidgetWithCursor(x, y);
+        if (widget_with_cursor) |new_w| {
+            new_w.onCursorEnter();
+        }
+    }
+}
+
+fn findWidgetWithCursor(x: u16, y: u16) ?*Widget {
+    var i: usize = 0;
+    while (i < widgets.len) : (i += 1) {
+        var w = widgets.uncheckedAt(i).*;
+        if (w.containsPoint(x, y)) {
+            return &w;
+        }
+    }
+
+    return null;
 }
 
 fn onKeyEvent(win: ?*GLFWwindow, key: c_int, scan_code: c_int, action: c_int, modifiers: c_int) callconv(.C) void {
@@ -123,5 +166,32 @@ fn onKeyEvent(win: ?*GLFWwindow, key: c_int, scan_code: c_int, action: c_int, mo
         };
 
         glfwSetWindowPos(win, new_x, new_y);
+    }
+}
+
+fn onMouseButtonEvent(win: ?*GLFWwindow, button: c_int, action: c_int, modifiers: c_int) callconv(.C) void {
+    if (button == GLFW_MOUSE_BUTTON_LEFT and action == GLFW_PRESS) {
+        if (widget_with_cursor) |wwc| {
+            if (widget_with_focus) |wwf| {
+                if (wwc != wwf) {
+                    wwf.onUnfocus();
+                }
+
+                // this needs to be more generic
+                wwc.onLeftMouseDown();
+                // the widget type itself needs to handle this logic
+                widget_with_focus = widget_with_cursor;
+            }
+        } else if (widget_with_focus) |wwf| {
+            wwf.onUnfocus();
+            widget_with_focus = null;
+        }
+    }
+}
+
+fn onDrop(win: ?*GLFWwindow, count: c_int, paths: [*c][*c]const u8) callconv(.C) void {
+    var i: usize = 0;
+    while (i < count) : (i += 1) {
+        warn("file: {s}\n", .{paths[i]});
     }
 }
