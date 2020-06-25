68e06a121a9d89790719632261c75de13bc6bac4
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-25 14:38:55 +0100

    Change widget instance member functions to pass by reference instead of value

--- src/widgets/rectangle.zig
+++ src/widgets/rectangle.zig
@@ -1,18 +1,31 @@
+const warn = @import("std").debug.warn;
 const Widget = @import("widget.zig").Widget;
+const BufferIndices = @import("../gl/buffer_indices.zig").BufferIndices;
 
 pub const Rectangle = struct {
     parent: ?*Widget,
 
-    pub fn onCursorEnter(self: Rectangle) void {}
-    pub fn onCursorLeave(self: Rectangle) void {}
-    pub fn onFocus(self: Rectangle) void {
+    pub fn insertIntoUi(self: *Rectangle) void {}
+
+    pub fn onCursorEnter(self: *Rectangle) void {}
+
+    pub fn onCursorLeave(self: *Rectangle) void {}
+
+    pub fn onLeftMouseDown(self: *Rectangle) void {
+        warn("left mouse button down on Rectangle\n", .{});
+    }
+
+    pub fn onFocus(self: *Rectangle) void {
         warn("Rectangle focus\n", .{});
     }
-    pub fn onUnfocus(self: Rectangle) void {
+
+    pub fn onUnfocus(self: *Rectangle) void {
         warn("Rectangle unfocus\n", .{});
     }
-    pub fn onWindowSizeChanged(self: Rectangle, width: c_int, height: c_int) void {}
-    pub fn containsPoint(self: Rectangle, x: u16, y: u16) bool {
+
+    pub fn onWindowSizeChanged(self: *Rectangle, width: c_int, height: c_int) void {}
+
+    pub fn containsPoint(self: *Rectangle, x: u16, y: u16) bool {
         return false;
     }
 };
--- src/widgets/search_bar.zig
+++ src/widgets/search_bar.zig
@@ -1,23 +1,46 @@
 const warn = @import("std").debug.warn;
 const Widget = @import("widget.zig").Widget;
+const BufferIndices = @import("../gl/buffer_indices.zig").BufferIndices;
 
-pub const SearchBar = struct {
+pub const SearchBar = packed struct {
+    const placeholder_text = "search...";
+    const text_offset_x: u16 = 30;
+    const text_offset_y: u16 = 44;
+
     parent: ?*Widget,
+    elapsed_ms: u64 = 0,
+    cursor_text_origin: u16 = text_offset_x,
+    cursor_position: u8 = 0,
+    search_string_length: u8 = 0,
+    is_focused: bool = false,
+    text_navigation_mode: bool = false,
+    input_handled: bool = false,
 
-    pub fn onCursorEnter(self: SearchBar) void {
+    pub fn insertIntoUi(self: *SearchBar) void {}
+
+    pub fn onCursorEnter(self: *SearchBar) void {
         warn("cursor entered the SearchBar\n", .{});
     }
-    pub fn onCursorLeave(self: SearchBar) void {
+
+    pub fn onCursorLeave(self: *SearchBar) void {
         warn("cursor left the SearchBar\n", .{});
     }
-    pub fn onFocus(self: SearchBar) void {
+
+    pub fn onLeftMouseDown(self: *SearchBar) void {
+        warn("left mouse button down on SearchBar\n", .{});
+    }
+
+    pub fn onFocus(self: *SearchBar) void {
         warn("SearchBar focus\n", .{});
     }
-    pub fn onUnfocus(self: SearchBar) void {
+
+    pub fn onUnfocus(self: *SearchBar) void {
         warn("SearchBar unfocus\n", .{});
     }
-    pub fn onWindowSizeChanged(self: SearchBar, width: c_int, height: c_int) void {}
-    pub fn containsPoint(self: SearchBar, x: u16, y: u16) bool {
+
+    pub fn onWindowSizeChanged(self: *SearchBar, width: c_int, height: c_int) void {}
+
+    pub fn containsPoint(self: *SearchBar, x: u16, y: u16) bool {
         return (x >= 20) and (x <= 940) and (y >= 20) and (y <= 20 + 34);
     }
 };
