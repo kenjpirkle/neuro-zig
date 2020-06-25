19eff7454bfff79c306cb2ba0a46fe0a6fba93d9
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-25 14:28:19 +0100

    Remove unnecessary namespaces and use reference to Widget instead of value

--- src/widgets/widget.zig
+++ src/widgets/widget.zig
@@ -1,5 +1,5 @@
-const rect = @import("rectangle.zig");
-const sb = @import("search_bar.zig");
+const Rectangle = @import("rectangle.zig").Rectangle;
+const SearchBar = @import("search_bar.zig").SearchBar;
 
 const WidgetTag = packed enum {
     Rectangle,
@@ -7,61 +7,61 @@ const WidgetTag = packed enum {
 };
 
 pub const Widget = union(WidgetTag) {
-    Rectangle: rect.Rectangle,
-    SearchBar: sb.SearchBar,
+    Rectangle: Rectangle,
+    SearchBar: SearchBar,
 
-    pub fn onCursorEnter(self: Widget) void {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| r.onCursorEnter(),
-            WidgetTag.SearchBar => |*s| s.onCursorEnter(),
+    pub fn onCursorEnter(self: *Widget) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onCursorEnter(),
+            .SearchBar => |*s| s.onCursorEnter(),
             else => unreachable,
         }
     }
 
-    pub fn onCursorLeave(self: Widget) void {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| r.onCursorLeave(),
-            WidgetTag.SearchBar => |*s| s.onCursorLeave(),
+    pub fn onCursorLeave(self: *Widget) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onCursorLeave(),
+            .SearchBar => |*s| s.onCursorLeave(),
             else => unreachable,
         }
     }
 
-    pub fn onFocus(self: Widget) void {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| r.onFocus(),
-            WidgetTag.Rectangle => |*r| r.onFocus(),
+    pub fn onFocus(self: *Widget) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onFocus(),
+            .SearchBar => |*s| s.onFocus(),
             else => unreachable,
         }
     }
 
-    pub fn onUnfocus(self: Widget) void {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| r.onUnfocus(),
-            WidgetTag.Rectangle => |*r| r.onUnfocus(),
+    pub fn onUnfocus(self: *Widget) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onUnfocus(),
+            .SearchBar => |*s| s.onUnfocus(),
             else => unreachable,
         }
     }
 
-    pub fn onLeftMouseDown(self: Widget) void {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| r.onLeftMouseDown(),
-            WidgetTag.SearchBar => |*s| s.onLeftMouseDown(),
+    pub fn onLeftMouseDown(self: *Widget) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onLeftMouseDown(),
+            .SearchBar => |*s| s.onLeftMouseDown(),
             else => unreachable,
         }
     }
 
-    pub inline fn onWindowSizeChanged(self: Widget, width: c_int, height: c_int) void {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| r.onWindowSizeChanged(width, height),
-            WidgetTag.SearchBar => |*s| s.onWindowSizeChanged(width, height),
+    pub inline fn onWindowSizeChanged(self: *Widget, width: c_int, height: c_int) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onWindowSizeChanged(width, height),
+            .SearchBar => |*s| s.onWindowSizeChanged(width, height),
             else => unreachable,
         }
     }
 
-    pub inline fn containsPoint(self: Widget, x: u16, y: u16) bool {
-        switch (self) {
-            WidgetTag.Rectangle => |*r| return r.containsPoint(x, y),
-            WidgetTag.SearchBar => |*s| return s.containsPoint(x, y),
+    pub inline fn containsPoint(self: *Widget, x: u16, y: u16) bool {
+        switch (self.*) {
+            .Rectangle => |*r| return r.containsPoint(x, y),
+            .SearchBar => |*s| return s.containsPoint(x, y),
             else => unreachable,
         }
     }
