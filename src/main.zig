fa384e8c0de043cc90fe7f1897fac569217cc4fe
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-25 14:40:11 +0100

    Change main.zig to utilize new UserInterface generic type

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,12 @@
 const Database = @import("database.zig").Database;
-const ui = @import("user_interface.zig");
+const UserInterface = @import("user_interface.zig").UserInterface;
 usingnamespace @import("c.zig");
 
 pub fn main() anyerror!void {
     var database = try Database.init("C:/Users/kenny/Desktop/neuro.db");
     defer database.deinit();
 
+    var ui: UserInterface(.{}) = undefined;
     try ui.init();
     defer ui.deinit();
 
