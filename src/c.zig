044775b0636afed7ea29b63a148747476437b676
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-25 14:27:38 +0100

    Change order of @cIncludes to fix compile error

--- src/c.zig
+++ src/c.zig
@@ -1,10 +1,10 @@
 pub usingnamespace @cImport({
     @cDefine("_NO_CRT_STDIO_INLINE", "1");
     @cInclude("stdio.h");
-    @cInclude("sqlite3.h");
     @cInclude("glad.h");
     @cInclude("glfw3.h");
     @cInclude("freetype.h");
+    @cInclude("sqlite3.h");
 });
 
 pub const math = @cImport({
