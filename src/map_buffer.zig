e92877896fa256187623b5fa733508b1dc0f54aa
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-26 23:28:26 +0100

    Remove unnecessary imports, remove count member and unnecessary cast

--- src/map_buffer.zig
+++ src/map_buffer.zig
@@ -1,25 +1,20 @@
-const mem = @import("std").mem;
 usingnamespace @import("c.zig");
-
-const map_flags: GLbitfield = GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
-const create_flags: GLbitfield = map_flags | GL_DYNAMIC_STORAGE_BIT;
 
 pub fn MapBuffer(comptime T: type, comptime size: usize) type {
     return struct {
         const Self = @This();
+        const map_flags: GLbitfield = GL_MAP_READ_BIT | GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT;
+        const create_flags: GLbitfield = map_flags | GL_DYNAMIC_STORAGE_BIT;
 
         data: []T,
-        count: usize,
         fence: GLsync,
 
         pub fn init(self: *Self, id: GLuint, target: GLenum) void {
             glBindBuffer(target, id);
             glNamedBufferStorage(id, @sizeOf(T) * size, null, create_flags);
-            const cptr = glMapNamedBufferRange(id, 0, @sizeOf(T) * size, map_flags);
-
-            self.data.ptr = @ptrCast([*]T, @alignCast(@alignOf([*]T), cptr));
-            self.data.len = size;
-            self.count = 0;
+            const ptr = glMapBufferRange(target, 0, @sizeOf(T) * size, map_flags);
+            self.data.ptr = @ptrCast([*]T, ptr);
+            self.data.len = 0;
             self.fence = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
         }
 
@@ -33,7 +28,13 @@ pub fn MapBuffer(comptime T: type, comptime size: usize) type {
         }
 
         pub fn append(self: *Self, values: []const T) void {
-            mem.copy(T, self.data[self.count..(self.count + values.len)], values);
+            const len = self.data.len;
+            self.data.len += values.len;
+            var i: usize = 0;
+            for (values) |*value| {
+                self.data[len + i] = values[i];
+                i += 1;
+            }
         }
     };
 }
