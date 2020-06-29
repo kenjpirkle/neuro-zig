0c2db52eee636709e3ae816ae024d157dfcc8e1a
Ken Pirkle <kenjpirkle@gmail.com>
2020-06-29 22:03:03 +0100

    Remove unncessary comptime modifier on BufferIndices fields

--- src/gl/buffer_indices.zig
+++ src/gl/buffer_indices.zig
@@ -1,6 +1,6 @@
 pub const BufferIndices = struct {
-    comptime quads: u32 = undefined,
-    comptime colours: u32 = undefined,
-    comptime colour_indices: u32 = undefined,
-    comptime draw_commands: u32 = undefined,
+    quads: u32 = 0,
+    colours: u32 = 0,
+    colour_indices: u32 = 0,
+    draw_commands: u32 = 0,
 };
