9b1da2c0b132275e7f90d2fc154ab0640cf0c6ef
Ken Pirkle <kenjpirkle@gmail.com>
2020-07-03 21:17:46 +0100

    Add onKeyEvent Widget function

--- src/widgets/widget.zig
+++ src/widgets/widget.zig
@@ -11,7 +11,7 @@ pub const Widget = union(WidgetTag) {
     Rectangle: Rectangle,
     SearchBar: SearchBar,
 
-    pub fn insertIntoUi(self: *Widget, ui: *UserInterface()) !void {
+    pub fn insertIntoUi(self: *Widget, ui: *UserInterface) !void {
         try switch (self.*) {
             .Rectangle => |*r| r.insertIntoUi(ui),
             .SearchBar => |*s| s.insertIntoUi(ui),
@@ -19,7 +19,7 @@ pub const Widget = union(WidgetTag) {
         };
     }
 
-    pub fn onCursorEnter(self: *Widget, ui: *UserInterface()) void {
+    pub fn onCursorEnter(self: *Widget, ui: *UserInterface) void {
         switch (self.*) {
             .Rectangle => |*r| r.onCursorEnter(),
             .SearchBar => |*s| s.onCursorEnter(ui),
@@ -27,7 +27,7 @@ pub const Widget = union(WidgetTag) {
         }
     }
 
-    pub fn onCursorLeave(self: *Widget, ui: *UserInterface()) void {
+    pub fn onCursorLeave(self: *Widget, ui: *UserInterface) void {
         switch (self.*) {
             .Rectangle => |*r| r.onCursorLeave(),
             .SearchBar => |*s| s.onCursorLeave(ui),
@@ -35,15 +35,31 @@ pub const Widget = union(WidgetTag) {
         }
     }
 
-    pub fn onFocus(self: *Widget, ui: *UserInterface()) void {
+    pub fn onKeyEvent(self: *Widget, ui: *UserInterface) void {
         switch (self.*) {
-            .Rectangle => |*r| r.onFocus(),
-            .SearchBar => |*s| s.onFocus(ui),
+            .Rectangle => |*r| r.onKeyEvent(self, ui),
+            .SearchBar => |*s| s.onKeyEvent(self, ui),
             else => unreachable,
         }
     }
 
-    pub fn onUnfocus(self: *Widget, ui: *UserInterface()) void {
+    pub fn onCharacterEvent(self: *Widget, ui: *UserInterface, codepoint: u32) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onCharacterEvent(self, ui, codepoint),
+            .SearchBar => |*s| s.onCharacterEvent(self, ui, codepoint),
+            else => unreachable,
+        }
+    }
+
+    pub fn onFocus(self: *Widget, ui: *UserInterface) void {
+        switch (self.*) {
+            .Rectangle => |*r| r.onFocus(),
+            .SearchBar => |*s| s.onFocus(self, ui),
+            else => unreachable,
+        }
+    }
+
+    pub fn onUnfocus(self: *Widget, ui: *UserInterface) void {
         switch (self.*) {
             .Rectangle => |*r| r.onUnfocus(),
             .SearchBar => |*s| s.onUnfocus(self, ui),
@@ -51,7 +67,7 @@ pub const Widget = union(WidgetTag) {
         }
     }
 
-    pub fn onLeftMouseDown(self: *Widget, ui: *UserInterface()) void {
+    pub fn onLeftMouseDown(self: *Widget, ui: *UserInterface) void {
         switch (self.*) {
             .Rectangle => |*r| r.onLeftMouseDown(),
             .SearchBar => |*s| s.onLeftMouseDown(self, ui),
@@ -59,7 +75,7 @@ pub const Widget = union(WidgetTag) {
         }
     }
 
-    pub inline fn onWindowSizeChanged(self: *Widget, ui: *UserInterface()) void {
+    pub inline fn onWindowSizeChanged(self: *Widget, ui: *UserInterface) void {
         switch (self.*) {
             .Rectangle => |*r| r.onWindowSizeChanged(ui),
             .SearchBar => |*s| s.onWindowSizeChanged(ui),
@@ -67,7 +83,7 @@ pub const Widget = union(WidgetTag) {
         }
     }
 
-    pub inline fn containsPoint(self: *Widget, ui: *UserInterface(), x: u16, y: u16) bool {
+    pub inline fn containsPoint(self: *Widget, ui: *UserInterface, x: u16, y: u16) bool {
         switch (self.*) {
             .Rectangle => |*r| return r.containsPoint(x, y),
             .SearchBar => |*s| return s.containsPoint(ui, x, y),
@@ -75,7 +91,7 @@ pub const Widget = union(WidgetTag) {
         }
     }
 
-    pub inline fn animate(self: *Widget, ui: *UserInterface(), time_delta: u64) void {
+    pub inline fn animate(self: *Widget, ui: *UserInterface, time_delta: u64) void {
         switch (self.*) {
             .Rectangle => unreachable,
             .SearchBar => |*s| s.animate(self, ui, time_delta),
