4655fd06af9224789726aa206703319f85081053
Ken Pirkle <kenjpirkle@gmail.com>
2020-08-09 16:26:24 +0100

    Move titlebar button logic from title_bar to their own structs

--- src/widgets/title_bar.zig
+++ src/widgets/title_bar.zig
@@ -5,190 +5,80 @@ const builtin = std.builtin;
 const mem = std.mem;
 const UserInterface = @import("../user_interface.zig").UserInterface;
 const Widget = @import("widget.zig").Widget;
-const BufferIndices = @import("../gl/buffer_indices.zig").BufferIndices;
+const MinimizeButton = @import("minimize_button.zig").MinimizeButton;
+const MaximizeRestoreButton = @import("maximize_restore_button.zig").MaximizeRestoreButton;
 const Quad = @import("../gl/quad.zig").Quad;
 const QuadTransform = @import("../gl/quad_transform.zig").QuadTransform;
 const QuadColourIndices = @import("../gl/quad_colour_indices.zig").QuadColourIndices;
 const Colour = @import("../gl/colour.zig").Colour;
 const DrawArraysIndirectCommand = @import("../gl/draw_arrays_indirect_command.zig").DrawArraysIndirectCommand;
 usingnamespace @import("../c.zig");
-const Index = @import("../buffer_indices.zig").TitleBarIndices;
+const Index = @import("../buffer_indices.zig").TitleBar;
+const WidgetIndex = @import("widget_index.zig");
 
 pub const TitleBar = packed struct {
     const Self = @This();
 
-    const titlebar_height: u16 = 24;
-    const button_width: u16 = 30;
+    pub const titlebar_height: u16 = 29;
+    pub const button_width: u16 = 46;
 
     elapsed_ns: u64 = 0,
     cursor_x: u16 = 0,
     cursor_y: u16 = 0,
 
     pub fn insertIntoUi(self: *Self, ui: *UserInterface) !void {
-        ui.quad_shader.quad_data.beginModify();
-        ui.quad_shader.colour_data.beginModify();
-        ui.quad_shader.colour_index_data.beginModify();
-
+        Index.MainRect.Quad = ui.quad_shader.quad_data.data.len;
         ui.quad_shader.quad_data.append(&[_]Quad{
-            // main bar
-            .{
-                .transform = .{
-                    .x = 0,
-                    .y = 0,
-                    .width = ui.width - (button_width * 3),
-                    .height = titlebar_height,
-                },
+            Quad.make(.{
+                .x = 0,
+                .y = 0,
+                .width = ui.width - (button_width * 3),
+                .height = titlebar_height,
                 .layer = 2,
                 .character = 1,
-            },
-            // minimize background
-            .{
-                .transform = .{
-                    .x = ui.width - (button_width * 3),
-                    .y = 0,
-                    .width = button_width,
-                    .height = titlebar_height,
-                },
-                .layer = 3,
-                .character = 1,
-            },
-            // minimize icon
-            .{
-                .transform = .{
-                    .x = ui.width - (button_width * 3) + 10,
-                    .y = 11,
-                    .width = 10,
-                    .height = 2,
-                },
-                .layer = 4,
-                .character = 1,
-            },
-            // maximize/restore
-            .{
-                .transform = .{
-                    .x = ui.width - (button_width * 2),
-                    .y = 0,
-                    .width = button_width,
-                    .height = titlebar_height,
-                },
-                .layer = 3,
-                .character = 1,
-            },
-            // close
-            .{
-                .transform = .{
-                    .x = ui.width - button_width,
-                    .y = 0,
-                    .width = button_width,
-                    .height = titlebar_height,
-                },
-                .layer = 3,
-                .character = 1,
-            },
+            }),
         });
-
+        Index.MainRect.Colour = @intCast(u8, ui.quad_shader.colour_data.data.len);
         ui.quad_shader.colour_data.append(&[_]Colour{
-            // titlebar
-            .{
-                .red = 1.0,
-                .green = 1.0,
-                .blue = 1.0,
-                .alpha = 0.0,
-            },
-            // minimize background
-            .{
-                .red = 0.35,
-                .green = 0.35,
-                .blue = 0.35,
-                .alpha = 0.0,
-            },
-            // minimize icon
-            .{
-                .red = 1.0,
-                .green = 1.0,
-                .blue = 1.0,
-                .alpha = 0.5,
-            },
-            // maximize/restore
-            .{
-                .red = 0.0,
-                .green = 1.0,
-                .blue = 0.0,
-                .alpha = 1.0,
-            },
-            // close
-            .{
-                .red = 1.0,
-                .green = 0.0,
-                .blue = 0.0,
-                .alpha = 1.0,
-            },
+            Colour.fromRgbaInt(255, 255, 255, 0),
         });
-
+        Index.MainRect.ColourIndices = ui.quad_shader.colour_index_data.data.len;
         ui.quad_shader.colour_index_data.append(&[_]QuadColourIndices{
             .{
-                .top_left = Index.MainRect.ColourId,
-                .bottom_left = Index.MainRect.ColourId,
-                .top_right = Index.MainRect.ColourId,
-                .bottom_right = Index.MainRect.ColourId,
-            },
-            .{
-                .top_left = Index.Minimize.ColourId,
-                .bottom_left = Index.Minimize.ColourId,
-                .top_right = Index.Minimize.ColourId,
-                .bottom_right = Index.Minimize.ColourId,
-            },
-            .{
-                .top_left = Index.MinimizeIcon.ColourId,
-                .bottom_left = Index.MinimizeIcon.ColourId,
-                .top_right = Index.MinimizeIcon.ColourId,
-                .bottom_right = Index.MinimizeIcon.ColourId,
-            },
-            .{
-                .top_left = Index.MaximizeRestore.ColourId,
-                .bottom_left = Index.MaximizeRestore.ColourId,
-                .top_right = Index.MaximizeRestore.ColourId,
-                .bottom_right = Index.MaximizeRestore.ColourId,
-            },
-            .{
-                .top_left = Index.Close.ColourId,
-                .bottom_left = Index.Close.ColourId,
-                .top_right = Index.Close.ColourId,
-                .bottom_right = Index.Close.ColourId,
+                .top_left = Index.MainRect.Colour,
+                .bottom_left = Index.MainRect.Colour,
+                .top_right = Index.MainRect.Colour,
+                .bottom_right = Index.MainRect.Colour,
             },
         });
-
-        ui.quad_shader.quad_data.endModify();
-        ui.quad_shader.colour_data.endModify();
-        ui.quad_shader.colour_index_data.endModify();
     }
 
-    pub fn onCursorEnter(self: *Self, ui: *UserInterface, x: u16, y: u16) void {
-        if (builtin.mode == .Debug) {
-            warn("cursor entered the TitleBar\n", .{});
-        }
-
-        const minimize_quad = &ui.quad_shader.quad_data.data[Index.Minimize.QuadId];
-        const maximize_quad = &ui.quad_shader.quad_data.data[Index.MaximizeRestore.QuadId];
-        const close_quad = &ui.quad_shader.quad_data.data[Index.Close.QuadId];
-
-        if (minimize_quad.containsX(x)) {
-            ui.quad_shader.colour_data.data[Index.Minimize.ColourId].alpha = 1.0;
-        } else {
-            ui.quad_shader.colour_data.data[Index.Minimize.ColourId].alpha = 0.0;
+    pub fn onCursorPositionChanged(self: *Self, ui: *UserInterface) void {
+        if (ui.quadAt(Index.MainRect.Quad).containsY(ui.cursor_y)) {
+            if (ui.quadAt(Index.MinimizeButton.Body.Quad).containsX(ui.cursor_x)) {
+                // cursor in MinimizeButton
+                ui.widgetAt(WidgetIndex.MinimizeButton).onCursorEnter(ui, ui.cursor_x, ui.cursor_y);
+            } else if (ui.quadAt(Index.MaximizeRestoreButton.Body.Quad).containsX(ui.cursor_x)) {
+                // cursor over MaximizeRestoreButton
+                ui.widgetAt(WidgetIndex.MaximizeRestoreButton).onCursorEnter(ui, ui.cursor_x, ui.cursor_y);
+            } else if (ui.quadAt(Index.CloseButton.Quad).containsX(ui.cursor_x)) {
+                // cursor over CloseButton
+            } else {
+                // cursor over TitleBar body
+                ui.widget_with_cursor = Widget.fromChild(self);
+                ui.input_handled = true;
+            }
         }
     }
 
-    pub fn onCursorLeave(self: *Self, ui: *UserInterface) void {
-        if (builtin.mode == .Debug) {
-            warn("cursor left the TitleBar\n", .{});
-        }
-    }
+    pub fn onCursorEnter(self: *Self, ui: *UserInterface, x: u16, y: u16) void {}
+
+    pub fn onCursorLeave(self: *Self, ui: *UserInterface) void {}
 
     pub fn onDrag(self: *Self, ui: *UserInterface, x: u16, y: u16) void {
         const cursor_delta_x: i32 = @intCast(i32, x) - @intCast(i32, self.cursor_x);
         const cursor_delta_y: i32 = @intCast(i32, y) - @intCast(i32, self.cursor_y);
-        glfwSetWindowPos(ui.window, ui.x + cursor_delta_x, ui.y + cursor_delta_y);
+        glfwSetWindowPos(ui.window, ui.window_position_x + cursor_delta_x, ui.window_position_y + cursor_delta_y);
     }
 
     pub fn onLeftMouseDown(self: *Self, widget: *Widget, ui: *UserInterface, x: u16, y: u16) void {
@@ -196,9 +86,9 @@ pub const TitleBar = packed struct {
             warn("left mouse button down on TitleBar\n", .{});
         }
 
-        const minimize_quad = &ui.quad_shader.quad_data.data[Index.Minimize.QuadId];
-        const maximize_quad = &ui.quad_shader.quad_data.data[Index.MaximizeRestore.QuadId];
-        const close_quad = &ui.quad_shader.quad_data.data[Index.Close.QuadId];
+        const minimize_quad = &ui.quad_shader.quad_data.data[Index.MinimizeButton.Body.Quad];
+        const maximize_quad = &ui.quad_shader.quad_data.data[Index.MaximizeRestoreButton.Body.Quad];
+        const close_quad = &ui.quad_shader.quad_data.data[Index.CloseButton.Quad];
 
         if (minimize_quad.containsX(x)) {
             glfwIconifyWindow(ui.window);
@@ -241,18 +131,7 @@ pub const TitleBar = packed struct {
 
     pub fn onCharacterEvent(self: *Self, widget: *Widget, ui: *UserInterface, codepoint: u32) void {}
 
-    pub fn onWindowSizeChanged(self: *Self, ui: *UserInterface) void {
-        const quads = ui.quad_shader.quad_data.data;
-
-        // main bar
-        quads[Index.MainRect.QuadId].transform.width = ui.width - (button_width * 3);
-        // minimize
-        quads[Index.Minimize.QuadId].transform.x = ui.width - (button_width * 3);
-        // maximize/restore
-        quads[Index.MaximizeRestore.QuadId].transform.x = ui.width - (button_width * 2);
-        // close
-        quads[Index.Close.QuadId].transform.x = ui.width - button_width;
-    }
+    pub fn onWindowSizeChanged(self: *Self, ui: *UserInterface) void {}
 
     pub fn containsPoint(self: *Self, ui: *UserInterface, x: u16, y: u16) bool {
         return (y >= 0) and (y <= titlebar_height);
