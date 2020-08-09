e71528e5de8dc902b45fb6e0ca15a7f1b53a926a
Ken Pirkle <kenjpirkle@gmail.com>
2020-08-09 16:22:38 +0100

    Add widget_index for application global widget indices

---
+++ src/widgets/widget_index.zig
@@ -0,0 +1,6 @@
+pub const Background: usize = 0;
+pub const TitleBar: usize = 1;
+pub const MinimizeButton: usize = 2;
+pub const MaximizeRestoreButton: usize = 3;
+pub const CloseButton: usize = 4;
+pub const SearchBar: usize = 5;
