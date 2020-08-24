80c4129b63552b0d81169e1a590f1b88f73e7551
Ken Pirkle <kenjpirkle@gmail.com>
2020-08-24 22:18:08 +0100

    Add additional global colours

--- src/widgets/widget_colours.zig
+++ src/widgets/widget_colours.zig
@@ -1,5 +1,9 @@
 const Colour = @import("../gl/colour.zig").Colour;
 
+pub const Background = struct {
+    pub const Default = Colour.fromRgbaInt(40, 44, 52, 255);
+};
+
 pub const TitleBar = struct {
     pub const MinimizeButton = struct {
         pub const Body = struct {
@@ -36,5 +40,60 @@ pub const TitleBar = struct {
             pub const Hover = Colour.fromRgbaInt(255, 255, 255, 180);
             pub const Pressed = Colour.fromRgbaInt(255, 255, 255, 255);
         };
+    };
+};
+
+pub const SearchBar = struct {
+    pub const MainRect = struct {
+        pub const Top = struct {
+            pub const Default = Colour.fromRgbaInt(47, 48, 59, 255);
+            pub const Hover = Colour.fromRgbaInt(53, 54, 65, 255);
+            pub const Focused = Colour.fromRgbaInt(53, 54, 65, 255);
+            pub const FocusedHover = Colour.fromRgbaInt(59, 60, 71, 255);
+        };
+        pub const Bottom = struct {
+            pub const Default = Colour.fromRgbaInt(52, 53, 64, 255);
+        };
+    };
+    pub const Shadow = struct {
+        pub const Top = struct {
+            pub const Default = Colour.fromRgbaInt(25, 25, 25, 250);
+        };
+        pub const Bottom = struct {
+            pub const Default = Colour.fromRgbaInt(25, 25, 25, 0);
+        };
+    };
+    pub const Highlight = struct {
+        pub const Top = struct {
+            pub const Default = Colour.fromRgbaInt(255, 255, 255, 0);
+        };
+        pub const Bottom = struct {
+            pub const Default = Colour.fromRgbaInt(255, 255, 255, 51);
+        };
+    };
+    pub const FocusHighlight = struct {
+        pub const Default = Colour.fromRgbaInt(50, 25, 255, 0);
+    };
+    pub const PlaceholderText = struct {
+        pub const Default = Colour.fromRgbaInt(217, 217, 255, 120);
+    };
+    pub const UserText = struct {
+        pub const Default = Colour.fromRgbaInt(255, 255, 255, 80);
+    };
+    pub const TextOverflow = struct {
+        pub const Left = struct {
+            pub const Top = Colour.fromRgbaInt(47, 48, 59, 0);
+            pub const Bottom = Colour.fromRgbaInt(52, 53, 64, 0);
+        };
+        pub const Right = struct {
+            pub const Top = Colour.fromRgbaInt(47, 48, 59, 0);
+            pub const Bottom = Colour.fromRgbaInt(52, 53, 64, 0);
+        };
+    };
+    pub const TextCursor = struct {
+        pub const Default = Colour.fromRgbaInt(50, 25, 255, 0);
+    };
+    pub const SelectionRect = struct {
+        pub const Default = Colour.fromRgbaInt(50, 50, 255, 0);
     };
 };
