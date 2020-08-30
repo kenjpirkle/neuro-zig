905699de92e9421f69884ad6ed914ca95b65ec95
Ken Pirkle <kenjpirkle@gmail.com>
2020-08-30 11:19:52 +0100

    Add additional utils function

--- src/utils.zig
+++ src/utils.zig
@@ -2,3 +2,7 @@ pub inline fn add(a: *u32, b: i32) void {
     const result = @intCast(u32, @as(i33, a.*) + b);
     a.* = result;
 }
+
+pub inline fn addU(a: u32, b: i32) u32 {
+    return @intCast(u32, @as(i33, a) + b);
+}
